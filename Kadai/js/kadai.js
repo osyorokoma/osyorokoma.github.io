77e07f4367dca4a0dfe40f80f6703695231fad41
osyorokoma <68935220+osyorokoma@users.noreply.github.com>
2020-07-29 12:34:13 +0900

    Add files via upload

---
+++ Kadai/js/kadai.js
@@ -0,0 +1,17 @@
+/*global $*/
+"use strict";
+
+$(function () {
+    $('a[href^=#]').click(function () {
+        var speed = 70;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position
+        }, speed, "swing");
+        return false;
+
+    });
+});
+
+
